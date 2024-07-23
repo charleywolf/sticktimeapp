8d4e7284e967c06aba95c36e4c8e7fb69db9d7ee
neoncodes <41237443+charleywolf@users.noreply.github.com>
2024-07-22 22:40:21 -0400

    fix (wsa): load more events

--- src/lib/wsa.ts
+++ src/lib/wsa.ts
@@ -1,33 +1,11 @@
-import {
-  getDateOneMonthFromNow,
-  getTodaysDate,
-  getTomorrowsDate,
-} from "./time";
+import { getDateOneMonthFromNow, getTodaysDate } from "./time";
 
 import { Sticktime } from "./fetch";
 
-function mapEventsToSticktimes(events: any): (Sticktime | null)[] {
-  return events.data.map((event: any) => ({
-    start: new Date(event.attributes.start),
-    end: new Date(event.attributes.end),
-    rink: "WSA",
-    price: 25,
-  }));
-}
-
 export default async function wsa(): Promise<Sticktime[]> {
+  const pageLength = process.env.NODE_ENV === "development" ? 500 : 500;
   const result = await fetch(
-    `
-https://apps.daysmartrecreation.com/dash/jsonapi/api/v1/events?cache[save]=false&page[size]=50&sort=end%2Cstart&include=summary%2Ccomments%2Cresource.facility.address%2Cresource.address%2CeventType.product.locations%2ChomeTeam.facility.address%2ChomeTeam.league.season.priorities.memberships%2ChomeTeam.league.season.priorities.activatedBySeasons%2ChomeTeam.programType%2ChomeTeam.product%2ChomeTeam.product.locations%2ChomeTeam.sport&filter[id__in]=122078%2C122052%2C121969&filter[start_date__gte]=${getTodaysDate()}&filter[start_date__lte]=${getDateOneMonthFromNow()}&filter[unconstrained]=1&filterRelations[comments.comment_type]=public&company=wsa`,
-    {
-      next: {
-        revalidate: 3600,
-      },
-    }
-  );
-
-  const result2 = await fetch(
-    `https://apps.daysmartrecreation.com/dash/jsonapi/api/v1/events?cache[save]=false&page[size]=50&sort=end%2Cstart&include=summary%2Ccomments%2Cresource.facility.address%2Cresource.address%2CeventType.product.locations%2ChomeTeam.facility.address%2ChomeTeam.league.season.priorities.memberships%2ChomeTeam.league.season.priorities.activatedBySeasons%2ChomeTeam.programType%2ChomeTeam.product%2ChomeTeam.product.locations%2ChomeTeam.sport&filter[id__in]=122054%2C121971%2C122080&filter[start_date__gte]=${getTomorrowsDate()}&filter[start_date__lte]=${getDateOneMonthFromNow()}&filter[unconstrained]=1&filterRelations[comments.comment_type]=public&company=wsa`,
+    `https://apps.daysmartrecreation.com/dash/jsonapi/api/v1/events?cache[save]=false&page[size]=${pageLength}&sort=start&company=wsa&filter[start__gte]=${getTodaysDate()}%2000%3A00%3A00&filter[start__lte]=${getDateOneMonthFromNow()}%2023%3A59%3A59&filter[resource.facility.my_sam_visible]=true&filter[eventType.code__not]=L&filter[eventType.id]=3&filter[resource.facility.id]=1&filterRelations[comments.comment_type]=public&include=homeTeam.league.programType%2CvisitingTeam.league.programType%2Csummary%2Cresource.facility%2CresourceArea%2Ccomments%2CeventType`,
     {
       next: {
         revalidate: 3600,
@@ -36,10 +14,35 @@ https://apps.daysmartrecreation.com/dash/jsonapi/api/v1/events?cache[save]=false
   );
 
   const events = await result.json();
-  const events2 = await result2.json();
 
-  return [
-    ...mapEventsToSticktimes(events),
-    ...mapEventsToSticktimes(events2),
-  ].filter((sticktime) => sticktime !== null);
+  const sticktimes: (Sticktime | null)[] = events.data.map((event: any) => {
+    try {
+      const element = events.included.find(
+        (item: any) => item.id === event.relationships.homeTeam.data.id
+      );
+
+      if (
+        element &&
+        element.type === "teams" &&
+        element.attributes.name.includes("Stick") &&
+        element.attributes.name.includes("Time")
+      ) {
+        return {
+          start: new Date(event.attributes.start),
+          end: new Date(event.attributes.end),
+          rink: "WSA",
+          price: 25,
+        };
+      } else {
+        return null;
+      }
+    } catch (e) {
+      console.error("Error parsing WSA event", e);
+      return null;
+    }
+  });
+
+  console.log(sticktimes.filter((sticktime) => sticktime !== null));
+
+  return sticktimes.filter((sticktime) => sticktime !== null);
 }
