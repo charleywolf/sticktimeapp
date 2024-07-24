5a578ef46f086a412ef7f7221f623e0bcf9d88c3
neoncodes <41237443+charleywolf@users.noreply.github.com>
2024-07-23 22:43:56 -0400

    docs: api key note

--- src/lib/rinks/icehutch.ts
+++ src/lib/rinks/icehutch.ts
@@ -6,6 +6,8 @@ import { Sticktime } from "../fetch";
 
 export default async function icehutch(): Promise<Sticktime[]> {
   const result = await fetch(
+    // NOTE: This API key can only be used for this endpoint and for public-facing requests, so it's okay. Also, there is no cost for additional calendar requests (see: https://developers.google.com/calendar/api/guides/quota).
+    // This was obtained from the publically facing icehutch.com website.
     `https://www.googleapis.com/calendar/v3/calendars/sandra@icehutch.com/events?key=AIzaSyA4x4wZZ0GGtnm-iR9iozrdpE49SwO28rY&singleEvents=True&maxResults=9999&timeMin=${getTodaysDate()}T00:00:00-07:00&timeZone=America/New_York`,
     {
       next: {
